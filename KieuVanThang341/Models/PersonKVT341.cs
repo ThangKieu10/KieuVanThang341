5feb54eabcb3551f81841b99ef883af0860689e2
Admin <Admin@LAPTOP-2LGM8HPC>
2021-10-06 07:20:35 +0700

    Create_Table_Person_KVT341

---
+++ KieuVanThang341/Models/PersonKVT341.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KieuVanThang341.Models
+{
+    public class KVT341
+    {
+        public int PersonId { get; set; }
+        public string PersionName { get; set; }
+
+    }
+}
